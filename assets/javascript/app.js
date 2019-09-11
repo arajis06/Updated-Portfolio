dcd28264fcdd0668336f881233ae273230a50966
ALEXIS RAJIS <arajis06@gmail.com>
2019-09-11 15:47:25 -0400

    added clicky-game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,21 +6,21 @@ $(function () {
 });
 
 //doughnut-MDbootstrap
-var ctxD = document.getElementById("doughnutChart").getContext('2d');
-var myLineChart = new Chart(ctxD, {
-  type: 'doughnut',
-  data: {
-    labels: ["HTML", "CSS", "Javascript", "JQuery", "Node.js"],
-    datasets: [{
-      data: [200, 170, 130, 90, 20],
-      backgroundColor: ["#9933CC", "#46BFBD", "#ec407a", "#949FB1", "#4D5360"],
-      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#03a9f4", "#4a148c", "#616774"]
-    }]
-  },
-  options: {
-    responsive: true
-  }
-});
+// var ctxD = document.getElementById("doughnutChart").getContext('2d');
+// var myLineChart = new Chart(ctxD, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ["HTML", "CSS", "Javascript", "JQuery", "Node.js"],
+//     datasets: [{
+//       data: [200, 170, 130, 90, 20],
+//       backgroundColor: ["#9933CC", "#46BFBD", "#ec407a", "#949FB1", "#4D5360"],
+//       hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#03a9f4", "#4a148c", "#616774"]
+//     }]
+//   },
+//   options: {
+//     responsive: true
+//   }
+// });
 
 function validateForm() {
   var name =  document.getElementById('name').value;
